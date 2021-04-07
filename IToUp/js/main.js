2d7afbb6b0ad4fe610a40e0d59dbc6561dea730e
Abolior <dimamatveev121@gmail.com>
2021-04-07 11:51:49 +0500

    mesage

--- IToUp/js/main.js
+++ IToUp/js/main.js
@@ -1,17 +1,34 @@
-const passwordEl = document.querySelector('.password');
-const cheackPass = document.querySelector('.cheackPass');
-const aufBtn= document.querySelector('.AufBtn');
-
-let isPass = true;
-
-function togglePass() {
-    if(isPass) {
-        passwordEl.setAttribute('type', 'text');
-        isPass = false;
-    } else {
-        passwordEl.setAttribute('type', 'password');
-        isPass = true;
+let slides = document.getElementsByClassName('header-pc-bg'),
+    dots = document.getElementsByClassName('header-pc-late'),
+    dotsWrap = document.querySelector('.header-pc-lates'),
+    slideIndex = 1;
+showSlides(slideIndex);
+function showSlides(n) {
+    if (n > slides.length) {
+        slideIndex = 1;
     }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('active')
+    }
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('active');
 }
-
-cheackPass.addEventListener('click', togglePass);
+function plusSlide(n) {
+    showSlides(slideIndex += n);
+}
+function currnetSlide(n) {
+    showSlides(slideIndex = n);
+}
+dotsWrap.addEventListener('click', function (event) {
+    for (let i = 0; i < dots.length + 1; i++) {
+        if (event.target.classList.contains('header-pc-late') && event.target == dots[i - 1]) {
+            currnetSlide(i);
+        }
+    }
+});
